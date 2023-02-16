23f57c04a33c7f2a08ad2385fcdf3fe9aaf98367
DaanVanHauwermeiren <daan.van.hauwermeiren@gmail.com>
2023-02-16 21:30:15 +0100

    cleanup of code in CP.py

--- src/Code/CP.py
+++ src/Code/CP.py
@@ -1,10 +1,25 @@
+import math
+import warnings
+
+import numpy as np
+
 from src.Code.QualityMeasures import *
 
-from scipy.optimize import root_scalar
-
-import math
-import numpy as np
-import warnings
+__all__ = [
+    "empirical_percentile",
+    "DISTscore",
+    "QRscore",
+    "CP",
+    "Taxonomy",
+    "MultisplitTaxonomy",
+    "MultiquantileTaxonomy",
+    "taxonomyFactory",
+    "conditionalCP",
+    "Conformalizer",
+    "PointConformalizer",
+    "EnsembleConformalizer",
+    "RandomEnsembleConformalizer",
+]
 
 __CONDITIONAL_MIN__ = 2
 
@@ -12,17 +27,18 @@ __CONDITIONAL_MIN__ = 2
 # helper function for the calculation of percentiles:
 def empirical_percentile(data, quantile):
     sorted = np.sort(data)
-    index = math.ceil(quantile * (1 + len(sorted))) - 1 # index corresponding to inflated quantile
+    index = math.ceil(quantile * (1 + len(sorted))) - 1  # index corresponding to inflated quantile
     return sorted[min(max(0, index), len(sorted) - 1)]
 
-############################
-############################
 
+############################
+############################
 
 
 def DISTscore(model, val_X, val_y):
     preds = model(val_X).squeeze()
     return np.abs(preds - val_y)
+
 
 def QRscore(model, val_X, val_y):
     preds = model(val_X)
@@ -31,6 +47,7 @@ def QRscore(model, val_X, val_y):
     preds = preds.squeeze()
     return np.maximum(preds[:, 0] - val_y, val_y - preds[:, 1])
 
+
 # General (inductive) conformalization method
 #
 # Arguments:
@@ -38,9 +55,10 @@ def QRscore(model, val_X, val_y):
 #   val_X: feature matrix (validation set)
 #   val_y: response variable (validation set)
 #   alpha: significance
-def CP(f, val_X, val_y, alpha = 0.1):
+def CP(f, val_X, val_y, alpha=0.1):
     score = f(val_X, val_y)
     return empirical_percentile(score, 1 - alpha)
+
 
 ################################################################################
 ################################################################################
@@ -52,12 +70,11 @@ def CP(f, val_X, val_y, alpha = 0.1):
 
 # Abstract class representing a taxonomy function
 class Taxonomy:
-
     def __init__(self, num):
         self.num = num
 
     def __str__(self):
-        return "AbstractTaxonomy-"+str(self.num)
+        return "AbstractTaxonomy-" + str(self.num)
 
     def train(self, X, y):
         pass
@@ -68,8 +85,8 @@ class Taxonomy:
     def split_count(self):
         return self.num
 
-class MultisplitTaxonomy(Taxonomy):
 
+class MultisplitTaxonomy(Taxonomy):
     def __init__(self, num):
         if num <= 1:
             raise Exception("Number of classes should be > 1")
@@ -77,7 +94,7 @@ class MultisplitTaxonomy(Taxonomy):
         self.splits = np.zeros(self.num - 1)
 
     def __str__(self):
-        return "MultisplitTaxonomy-"+str(self.num)
+        return "MultisplitTaxonomy-" + str(self.num)
 
     def train(self, X, y):
         sorted = np.sort(y)
@@ -86,8 +103,8 @@ class MultisplitTaxonomy(Taxonomy):
     def classify(self, X, y):
         return np.array([self.num - np.sum(self.splits > val) for val in y])
 
-class MultiquantileTaxonomy(Taxonomy):
 
+class MultiquantileTaxonomy(Taxonomy):
     def __init__(self, quantiles):
         if len(quantiles) < 1:
             raise Exception("Number of classes should be > 1")
@@ -96,13 +113,14 @@ class MultiquantileTaxonomy(Taxonomy):
         self.__quantiles = quantiles
 
     def __str__(self):
-        return "MultiquantileTaxonomy-"+str(self.__quantiles)
+        return "MultiquantileTaxonomy-" + str(self.__quantiles)
 
     def train(self, X, y):
         self.splits = np.quantile(y, self.__quantiles)
 
     def classify(self, X, y):
         return np.array([self.num - np.sum(self.splits > val) for val in y])
+
 
 def taxonomyFactory(type, params):
     if type == "multisplit" or type == "split":
@@ -112,6 +130,7 @@ def taxonomyFactory(type, params):
 
     return None
 
+
 # Conditional (inductive) conformalization method
 #
 # Arguments:
@@ -120,7 +139,7 @@ def taxonomyFactory(type, params):
 #   val_X: feature matrix (validation set)
 #   val_y: response variable (validation set)
 #   alpha: significance
-def conditionalCP(f, tax, val_X, val_y, alpha = 0.1, suppress_verbosity = False):
+def conditionalCP(f, tax, val_X, val_y, alpha=0.1, suppress_verbosity=False):
     score = f(val_X, val_y)
     taxonomy = tax.classify(val_X, val_y)
 
@@ -136,11 +155,16 @@ def conditionalCP(f, tax, val_X, val_y, alpha = 0.1, suppress_verbosity = False)
             #########
 
             if len(score_t) == 0:
-                warnings.warn("Class "+str(t)+" contains 0 instances. It might be better to use different splits. Defaulting to threshold 0.")
+                warnings.warn(
+                    "Class "
+                    + str(t)
+                    + " contains 0 instances. It might be better to use different splits. Defaulting to threshold 0."
+                )
             else:
                 thresholds[t] = empirical_percentile(score_t, 1 - alpha)
 
     return thresholds
+
 
 ################################################################################
 ################################################################################
@@ -150,8 +174,8 @@ def conditionalCP(f, tax, val_X, val_y, alpha = 0.1, suppress_verbosity = False)
 ################################################################################
 ################################################################################
 
-class Conformalizer():
 
+class Conformalizer:
     def __init__(self, container, model, train_model, disconformity_score):
         self.container = container
         self.model = model
@@ -164,7 +188,7 @@ class Conformalizer():
             for i in range(1, self.container.taxonomy_func().split_count() + 1):
                 self.conditional_thresholds[i] = np.Inf
 
-    def train(self, x, y, suppress_verbosity = False):
+    def train(self, x, y, suppress_verbosity=False):
         if self.container.cp_mode():
             self.threshold = empirical_percentile(self.__score(self.train_model, x, y), 1 - self.container.alpha())
             if self.container.verbosity() > 1 and not suppress_verbosity:
@@ -172,9 +196,18 @@ class Conformalizer():
                 print()
 
     # NOT USED YET
-    def train_conditional(self, x, y, suppress_verbosity = False):
+    def train_conditional(self, x, y, suppress_verbosity=False):
         if self.container.cp_mode():
-            self.conditional_thresholds.update(conditionalCP(lambda x, y: self.__score(self.train_model, x, y), self.container.taxonomy_func(), x, y, self.container.alpha(), suppress_verbosity))
+            self.conditional_thresholds.update(
+                conditionalCP(
+                    lambda x, y: self.__score(self.train_model, x, y),
+                    self.container.taxonomy_func(),
+                    x,
+                    y,
+                    self.container.alpha(),
+                    suppress_verbosity,
+                )
+            )
 
     def apply(self, x, y):
         pass
@@ -182,9 +215,9 @@ class Conformalizer():
     def apply_conditional(self, x, y):
         pass
 
-class PointConformalizer(Conformalizer):
 
-    def __init__(self, container, model, train_model = None):
+class PointConformalizer(Conformalizer):
+    def __init__(self, container, model, train_model=None):
         super().__init__(container, model, train_model, DISTscore)
 
     def apply(self, x, y):
@@ -223,18 +256,19 @@ class PointConformalizer(Conformalizer):
         upper = preds + threshold
 
         for t in sorted(self.conditional_thresholds.keys()):
-            index = (taxonomy == t)
+            index = taxonomy == t
             if np.sum(index) > __CONDITIONAL_MIN__:
                 self.container["conditional_cp_cov", t] = coverage(lower[index], upper[index], y[index])
                 self.container["conditional_cp_width", t] = MPIW(lower[index], upper[index], y[index])
                 self.container["conditional_cp_perf", t] = self.container.perf_func(y[index], preds[index])
+
 
 class EnsembleConformalizer(Conformalizer):
 
     # Arguments:
     #   ensemble_function: Function that takes a list of models and creates an ensemble prediction.
     #                      Should output a NumPy array of the form [lower_quantiles, upper_quantiles, predictions]
-    def __init__(self, container, ensemble_func, train_ensemble_func = None):
+    def __init__(self, container, ensemble_func, train_ensemble_func=None):
         super().__init__(container, ensemble_func, train_ensemble_func, QRscore)
 
     def apply(self, x, y):
@@ -292,7 +326,7 @@ class EnsembleConformalizer(Conformalizer):
 
         # Check performance WITHOUT conformalization
         for t in sorted(self.conditional_thresholds.keys()):
-            index = (taxonomy == t)
+            index = taxonomy == t
             if np.sum(index) > __CONDITIONAL_MIN__:
                 self.container["conditional_cov", t] = coverage(lower[index], upper[index], y[index])
                 self.container["conditional_width", t] = MPIW(lower[index], upper[index], y[index])
@@ -306,18 +340,18 @@ class EnsembleConformalizer(Conformalizer):
             upper = upper + threshold
 
             for t in sorted(self.conditional_thresholds.keys()):
-                index = (taxonomy == t)
+                index = taxonomy == t
                 if np.sum(index) > __CONDITIONAL_MIN__:
                     self.container["conditional_cp_cov", t] = coverage(lower[index], upper[index], y[index])
                     self.container["conditional_cp_width", t] = MPIW(lower[index], upper[index], y[index])
                     self.container["conditional_cp_perf", t] = self.container.perf_func(y[index], preds[index])
 
-class RandomEnsembleConformalizer(EnsembleConformalizer):
 
+class RandomEnsembleConformalizer(EnsembleConformalizer):
     def train(self, x, y):
         total = 0
         for i in range(self.container.num()):
-            super().train(x, y, suppress_verbosity = True)
+            super().train(x, y, suppress_verbosity=True)
             total += self.threshold
         self.threshold = total / self.container.num()
 
@@ -330,6 +364,6 @@ class RandomEnsembleConformalizer(EnsembleConformalizer):
         super().train_conditional(x, y)
         total = self.conditional_thresholds
         for i in range(self.container.num() - 1):
-            super().train_conditional(x, y, suppress_verbosity = True)
+            super().train_conditional(x, y, suppress_verbosity=True)
             total = {k: total.get(k, 0) + self.conditional_thresholds.get(k, 0) for k in set(total)}
         self.conditional_thresholds = {k: total.get(k, 0) / self.container.num() for k in set(total)}
